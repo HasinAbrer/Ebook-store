63a8651efd0970bbb598123a0f495db9a199754f
HasinAbrer <hasinjunior12@gmail.com>
2025-06-08 20:41:22 +0600

    again added cart issue resolve

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Children, useState } from "react";
 import { Link } from "react-router-dom";
 import { HiBars3CenterLeft } from "react-icons/hi2";
 import { IoSearchOutline } from "react-icons/io5";
@@ -6,6 +6,7 @@ import { HiOutlineUser } from "react-icons/hi";
 import { HiOutlineHeart } from "react-icons/hi";
 import { HiOutlineShoppingCart } from "react-icons/hi";
 import avatarImg from "../assets/avatar.png";
+import { useSelector } from "react-redux";
 
 const navigation = [
   { name: "Dashboard", href: "/dashboard" },
@@ -16,6 +17,7 @@ const navigation = [
 
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const cartItems = useSelector (state => state.cart.cartItems);
 
   const currentUser = false;
   return (
@@ -85,7 +87,12 @@ const Navbar = () => {
             className="bg-primary p-1 sm:px-6 px-2 flex items-center rounded-md"
           >
             <HiOutlineShoppingCart className="" />
+            {
+              cartItems.length > 0 ? <span className="text-sm font semibold sm:ml-1">{cartItems.length}</span> : <span className="text-sm font semibold sm:ml-1">0</span>
+            }
+            
             <span className="text-sm font semibold sm:ml-1">0</span>
+
           </Link>
         </div>
       </nav>
--- frontend/src/redux/features/cart/cartSlice.js
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -1,26 +1,42 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+import Swal from "sweetalert2";
 
 const initialState = {
-  cartItems: []
-}
+  cartItems: [],
+};
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState: initialState,
-    reducers: {
-        addToCart: (state, action) => {
-            const existingItem = state.cartItems.find(item => item._id === action.payload.id);
-            if(!existingItem) {
-                state.cartItems.push(action.payload)
-                alert("Item added successfully");
-            } else (
-                alert("Item already exists")
-            )
-        }        
-    }
+  name: "cart",
+  initialState: initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const existingItem = state.cartItems.find(
+        (item) => item._id === action.payload._id
+      );
+      if (!existingItem) {
+        state.cartItems.push(action.payload);
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Product added to the Cart",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      } else (
+        Swal.fire({
+          title: "Product already in the Cart",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Ok!",
+        })
+        );
+    },
+  },
 });
 
 // export the actions
-export const {addToCart} = cartSlice.actions
-export default cartSlice.reducer
-
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
