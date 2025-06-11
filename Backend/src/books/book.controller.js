c3e0c849b49c1f315a083cdf7af1d2c55a87a565
Moloy Banerjee <moloyswe20@gmail.com>
2025-06-11 16:54:53 +0600

    set up all book routes and controllers

--- Backend/src/books/book.controller.js
+++ Backend/src/books/book.controller.js
@@ -13,7 +13,7 @@ const postABook= async (req, res) => {
 
 const getAllBooks = async (req, res) => {
   try {
-    const books = await Book.find();
+    const books = await Book.find().sort({ createdAt: -1 }); // Sort by createdAt in descending order
     res.status(200).send({ message: "Books fetched successfully", books });
   } catch (error) {
     console.error("Error fetching books", error);
@@ -21,7 +21,58 @@ const getAllBooks = async (req, res) => {
   }
 }
 
+const getSingleBook = async (req, res) => {
+  try {
+    const {id} = req.params;
+    const book = await Book.findById(id);
+    if (!book) {
+      res.status(404).send({ message: "Book not found" });
+    }
+    res.status(200).send(book);
+  } catch (error) {
+    console.error("Error fetching book", error);
+    res.status(500).send({ message: "Failed to fetch book"});
+  }
+}
+
+const UpdateBook= async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedBook = await Book.findByIdAndUpdate(id, req.body, { new: true });
+    if (!updatedBook) {
+      return res.status(404).send({ message: "Book not found" });
+    }
+    res.status(200).send({
+        message: "Book updated successfully",
+         book: updatedBook
+        })
+  } catch (error) {
+    console.error("Error updating book", error);
+    res.status(500).send({ message: "Failed to update book", error });
+  }
+}
+
+const DeleteABook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedBook = await Book.findByIdAndDelete(id);
+    if (!deletedBook) {
+      return res.status(404).send({ message: "Book not found" });
+    }
+    res.status(200).send({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting book", error);
+    res.status(500).send({ message: "Failed to delete book", error });
+  }
+}
+
+
+
+
 module.exports = {
   postABook,
-  getAllBooks
+  getAllBooks,
+  getSingleBook,
+  UpdateBook,
+  DeleteABook
 };
