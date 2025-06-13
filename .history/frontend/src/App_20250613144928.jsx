a1d895c7a21122f62de2aaa62f07988d05b15d9c
Moloy Banerjee <moloyswe20@gmail.com>
2025-06-13 15:02:03 +0600

    Created the Loading component

--- .history/frontend/src/App_20250613144928.jsx
+++ .history/frontend/src/App_20250613144928.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import { AuthProvider } from './context/AuthContext'
+import { AuthProvide, AuthProvider } from './context/AuthContext'
 import { useEffect, useState } from 'react'
 import Loading from './components/Loading'
 
@@ -28,13 +28,11 @@ function App() {
 
   return (
     <>
-      <AuthProvider>
         <Navbar />
         <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
           <Outlet />
         </main>
         <Footer />
-      </AuthProvider>
 
     </>
   )
