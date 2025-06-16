cea512fbbb94a174300a3bac4d916e3c330024f8
Moloy Banerjee <moloyswe20@gmail.com>
2025-06-16 23:32:48 +0600

    Admin Login Page Created

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -8,6 +8,8 @@ import CheckoutPage from "../pages/books/CheckoutPage";
 import SingleBook from "../pages/books/SingleBook";
 import OrderPage from "../pages/books/OrderPage";
 import PrivateRoute from "./PrivateRoute";
+import AdminRoute from "./AdminRoute";
+import AdminLogin from "../components/AdminLogin";
 
 const router = createBrowserRouter([
   {
@@ -20,7 +22,7 @@ const router = createBrowserRouter([
         },
         {
             path: "/orders",
-            element: <OrderPage/>
+            element: <PrivateRoute><OrderPage/></PrivateRoute>
         },
         {
             path: "/about",
@@ -48,6 +50,32 @@ const router = createBrowserRouter([
         }
      ]
     },
+    {
+      path: "/admin",
+      element: <AdminLogin/>,
+    },
+    {
+  path: "/dashboard",
+  element: <AdminRoute><div>Dashboard</div></AdminRoute>,
+  children: [
+    {
+      path: "",
+      element: <AdminRoute><div>Dashboard Home</div></AdminRoute>
+    },
+    {
+      path: "add-new-book",
+      element: <AdminRoute><div>Add New Book</div></AdminRoute>
+    },
+    {
+      path: "edit-book/:id",
+      element: <AdminRoute><div>Edit Book</div></AdminRoute>
+    },
+    {
+      path: "manage-books",
+      element: <AdminRoute><div>Manage Books</div></AdminRoute>
+    }
+  ]
+}
 ]);
 
 export default router;
